23362bda63d55a09d53373eac798b5839fa113a4
Azag <Azag@users.noreply.github.com>
2022-06-16 12:47:26 +0200

    fix

--- server.js
+++ server.js
@@ -1,14 +1,10 @@
-const { application } = require('express');
 const express = require('express');
-const cors = require('cors');
-
 const app = express();
-//app.use(cors({origin: "*"}))
 
 const { quotes } = require('./data');
 const { getRandomElement, getByAuthor } = require('./utils');
 
-const PORT = process.env.PORT || 4001;
+const PORT = process.env.PORT ?? 4001;
 
 app.use(express.static('public'));
 app.use(express.json())
@@ -16,9 +12,6 @@ app.use(express.json())
 
 app.listen(PORT, () => console.log(`Server up at port:${PORT}`));
 
-// app.get('/', (res, req, next) => {
-//     //res.send(randomQuote);
-// })
 
 app.get('/api/quotes/random', (req, res, next) => {
     const randomQuote = getRandomElement(quotes);
